cba38b1c17cb3f16132c69688ef6311927252b24
Pavel Lebedev <maxwinrate@gmail.com>
2020-02-27 21:09:40 +0200

    Chain of Responsibility

---
+++ CoR/Server.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+
+namespace app\CoR;
+
+
+class Server {
+	private $users = [];
+	private $middleware;
+
+	public function setMiddleware(Middleware $middleware) {
+		$this->middleware = $middleware;
+	}
+
+	public function logIn(string $email, string $password) {
+		if ($this->middleware->check($email, $password)) {
+			echo "Server: Authorization has been successful!\n";
+			return true;
+		}
+
+		return false;
+	}
+
+	public function register(string $email, string $password): void {
+		$this->users[$email] = $password;
+	}
+
+	public function hasEmail(string $email): bool {
+		return isset($this->users[$email]);
+	}
+
+	public function isValidPassword(string $email, string $password): bool {
+		return $this->users[$email] === $password;
+	}
+}
--- CoR/impl.php
+++ CoR/impl.php
@@ -1,3 +1,23 @@
 <?php declare(strict_types=1);
 
+namespace app\CoR;
+require_once "../vendor/autoload.php";
 
+$server = new Server();
+$server->register("admin@example.com", "admin_pass");
+$server->register("user@example.com", "user_pass");
+
+$middleware = new ThrottlingMiddleware(2);
+$middleware
+	->linkWith(new UserExistsMiddleware($server))
+	->linkWith(new RoleCheckMiddleware);
+
+$server->setMiddleware($middleware);
+
+do {
+	echo "\nEnter your email:\n";
+	$email = readline();
+	echo "Enter your password:\n";
+	$password = readline();
+	$success = $server->logIn($email, $password);
+} while (!$success);
